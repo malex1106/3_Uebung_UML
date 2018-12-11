7fc7ab7a76dc864828f644e70334148613de8260
malex1106 <malex1106@gmail.com>
2018-12-11 19:23:27 +0100

    GUI: Grundschema festgelegt.

--- src/com/uebung3/Controller/MainController.java
+++ src/com/uebung3/Controller/MainController.java
@@ -3,15 +3,28 @@ package com.uebung3.Controller;
 import com.uebung3.Classes.SchuleClass;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+    @FXML
+    private BorderPane schoolPanel;
+
+    @FXML
+    private AnchorPane abtAddPanel;
+
+    @FXML
+    private TextField abtAddTextfield;
 
     @FXML
     private ListView<String> listViewAbteilungen;
@@ -21,7 +34,7 @@ public class MainController implements Initializable {
 
     private static SchuleClass schule;
 
-    private static ObservableList<String> abteilungslist = FXCollections.observableArrayList();
+    private ObservableList<String> abteilungslist = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -40,6 +53,30 @@ public class MainController implements Initializable {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    void abtAddButtonClicked(ActionEvent event) {
+        this.schoolPanel.setDisable(true);
+        this.abtAddPanel.setVisible(true);
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    // Abteilung Hinzufügen - Panel
+    ////////////////////////////////////////////////////////////////////////
+
+    @FXML
+    void abtAddAddButton(ActionEvent event) {
+        this.abteilungslist.add(abtAddTextfield.getText());
+
+        this.abtAddPanel.setVisible(false);
+        this.schoolPanel.setDisable(false);
+    }
+
+    @FXML
+    void abtAddExitButtonClicked(ActionEvent event) {
+        this.abtAddPanel.setVisible(false);
+        this.schoolPanel.setDisable(false);
     }
 
     public static SchuleClass getSchule() { return schule; }
